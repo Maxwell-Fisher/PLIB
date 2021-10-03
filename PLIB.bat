2fe4a8a59c2531044b0801ae392863946b74c047
Maxwell Fisher <54481244+Maxwell-Fisher@users.noreply.github.com>
2021-10-03 00:40:48 -0600

    Can now load from files

--- PLIB.bat
+++ PLIB.bat
@@ -6,14 +6,43 @@ cls
 
 set lineCount=1
 
-:get
+:getMode
+echo Would you like to load an existing file or write a new program? (load/new)
+set /p mode="> "
+if /i "!mode!"=="load" goto getExisting
+if /i "!mode!"=="new" cls && goto getNew
+cls
+echo Unknown option "!mode!", please enter either LOAD or NEW
+goto getMode
+
+:getExisting
+cls
+echo Files: 
+for %%f in (*.plib) do (
+if "%%~xf"==".plib" set file=%%f
+echo !file:~0,-5!
+)
+echo.
+echo Type the name of the file that you want to run:
+set /p fileChoice="> "
+if not exist "!fileChoice!.plib" set fileChoice=!fileChoice:~0,-5!
+if not exist "!fileChoice!.plib" goto getExisting
+for /f "tokens=*" %%a in (!fileChoice!.plib) do (
+set line_!lineCount!=%%a
+if "%%a"=="end" goto go
+set /a lineCount=lineCount+1
+)
+goto go
+rem goto getExisting
+
+:getNew
 set current=000!lineCount!
 set current=!current:~-4!
 set /p input="!current!> "
 set line_!lineCount!=!input!
 if "!input!"=="end" goto go
 set /a lineCount=lineCount+1
-goto get
+goto getNew
 
 :go
 cls
@@ -24,7 +53,7 @@ set a=0
 :main
 set /a current=current+1
 
-
+if "!line_%current%!"=="" goto main
 if "!line_%current%:~0,3!"=="end" goto end
 if "!line_%current%:~0,5!"=="clear" cls && goto main
 if "!line_%current%:~0,8!"=="println " echo !line_%current%:~8,64! && goto main
@@ -34,6 +63,8 @@ if "!line_%current%:~0,3!"=="inc" set /a a=a+1 && goto main
 if "!line_%current%:~0,3!"=="dec" set /a a=a-1 && goto main
 if "!line_%current%:~0,3!"=="val" echo|set /p="!a!" && goto main
 if "!line_%current%:~0,4!"=="wait" ping 127.1 -n 2 > nul && goto main
+
+rem echo Unknown command "!line_%current%!"
 
 goto main
 
