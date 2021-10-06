ea0edf299e221b5dfe3c74b026aa4a63a3df5f7d
Maxwell Fisher <54481244+Maxwell-Fisher@users.noreply.github.com>
2021-10-06 11:19:39 -0600

    Added the `col` command, used to change the console colour

--- PLIB.bat
+++ PLIB.bat
@@ -73,6 +73,8 @@ if "!line_%current%:~0,3!"=="end" (
     <nul set /p="!a!"
 ) else if "!line_%current%:~0,4!"=="wait" (
     ping 127.1 -n 2 > nul
+) else if "!line_%current%:~0,4!"=="col " (
+    color !line_%current%:~4,2!
 )
 goto main
 
