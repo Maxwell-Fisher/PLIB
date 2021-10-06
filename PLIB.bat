f07c929ce2f31f19e76816a0a96a02f41bfc632f
Maxwell Fisher <54481244+Maxwell-Fisher@users.noreply.github.com>
2021-10-06 11:58:59 -0600

    Removed the `val` command, replaced by printing `$val`

--- PLIB.bat
+++ PLIB.bat
@@ -59,18 +59,20 @@ if "!line_%current%:~0,3!"=="end" (
 ) else if "!line_%current%:~0,5!"=="clear" (
     cls
 ) else if "!line_%current%:~0,8!"=="println " (
-    echo !line_%current%:~8,64!
+    set output=!line_%current%:~8,64!
+    set output=!output:$val=%a%!
+    echo !output!
 ) else if "!line_%current%:~0,6!"=="print " (
-    <nul set /p="!line_%current%:~6,64!"
+    set output=!line_%current%:~6,64!
+    set output=!output:$val=%a%!
+    <nul set /p="!output!"
 ) else if "!line_%current%:~0,5!"=="goto " (
     set current=!line_%current%:~5,64!
     set /a current=current-1
 ) else if "!line_%current%:~0,3!"=="inc" (
-    set /a a=a+1
+    set /a a+=1
 ) else if "!line_%current%:~0,3!"=="dec" (
-    set /a a=a-1
-) else if "!line_%current%:~0,3!"=="val" (
-    <nul set /p="!a!"
+    set /a a=a-=1
 ) else if "!line_%current%:~0,4!"=="wait" (
     ping 127.1 -n 2 > nul
 ) else if "!line_%current%:~0,4!"=="col " (
